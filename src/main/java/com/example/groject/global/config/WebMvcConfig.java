06c59b275602f72e1ec897657a2be3e3b7e89c4c
munjeongmin <3330kys@dsm.hs.kr>
2022-10-06 13:01:16 +0900

    ADD :: Cors setting, base time entity

---
+++ src/main/java/com/example/groject/global/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.groject.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
