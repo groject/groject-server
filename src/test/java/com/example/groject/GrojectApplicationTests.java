91056370508b63e0825904e2b9ac08a9dd79b811
munjeongmin <3330kys@dsm.hs.kr>
2022-10-06 12:57:53 +0900

    ADD :: application.yml

---
+++ src/test/java/com/example/groject/GrojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
