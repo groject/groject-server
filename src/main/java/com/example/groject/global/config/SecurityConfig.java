55351d529d7295cd6d4dc2c53f0c219305cc1963
munjeongmin <3330kys@dsm.hs.kr>
2022-10-07 03:25:15 +0900

    ADD :: Cors config

--- src/main/java/com/example/groject/global/config/SecurityConfig.java
+++ src/main/java/com/example/groject/global/config/SecurityConfig.java
@@ -12,9 +12,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -33,7 +30,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors()
-                .configurationSource(configurationSource())
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
@@ -55,17 +51,5 @@ public class SecurityConfig {
 
                 .and()
                 .build();
-    }
-    
-    @Bean
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:3030");
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
     }
 }
