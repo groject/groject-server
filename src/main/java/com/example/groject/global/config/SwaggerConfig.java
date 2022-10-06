bfb2a082a2ebe97b05f5610f5c713eee602cdb56
munjeongmin <3330kys@dsm.hs.kr>
2022-10-06 17:11:28 +0900

    EDIT :: Redis delete, swagger

--- src/main/java/com/example/groject/global/config/SwaggerConfig.java
+++ src/main/java/com/example/groject/global/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(
                         new Info().title("그로젝트 API")
-                                .description("그로젝트 Api 명세서입니다.")
+                                .description("")
                                 .version("v1")
                 );
     }
